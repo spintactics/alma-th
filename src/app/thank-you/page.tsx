74b272decc4082cefd001d9b0e51580ea4f585a0
spintactics <rainfang2014@gmail.com>
2024-11-16 03:40:22 -0500

    feat(eng-08): Thank you page styling

--- src/app/thank-you/page.tsx
+++ src/app/thank-you/page.tsx
@@ -2,13 +2,18 @@ import Link from "next/link";
 
 export default function ThankYouPage() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen text-center p-4">
-      <h2 className="text-3xl font-bold mb-4">Thank You</h2>
-      <p className="text-lg text-gray-600 mb-8">
-        Your information was submitted to our team of immigration attorneys...
+    <div className="flex flex-col items-center justify-center min-h-screen text-center p-4 bg-white">
+      <img
+        src="/assets/i-file.png"
+        alt="i file"
+        className="h-14 mb-4 mx-auto"
+      />
+      <h2 className="text-3xl text-black font-bold mb-5">Thank You</h2>
+      <p className="text-lg text-black font-semibold mb-8" style={{ width: '40vw' }}>
+        Your information was submitted to our team of immigration attorneys. Expect an email from hello@tryalma.ai.
       </p>
       <Link href="/">
-        <button className="bg-gray-800 text-white py-3 px-6 rounded-lg">
+        <button className="bg-gray-800 text-white py-3 px-6 rounded-lg" style={{ width: '25vw' }}>
           Go Back to Homepage
         </button>
       </Link>
